c3272fabd65868750e653741b84d4531ff07f625
Andres <jandres.ttorres2003@gmail.com>
2026-08-18 18:41:30 -0500

    feat: imprimir resta

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def sumar(a:int, b:int) -> int:
+def sumar(a: int, b: int) -> int:
     return a + b
+    """
+    Suma dos numeros
+    """
 
-def restar(a:int, b:int) -> int:
+def restar(a: int, b: int) -> int:
     return a - b
 
-print(sumar(2,3))
-#knvekcnelcnelcnel
+print(sumar(2, 3))
+
+print(restar(3,5))
+# knvekcnelcnelcnel
